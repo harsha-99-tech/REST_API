6da3b510b6867da54f228afdbd226b662c81a23b
harsha-99-tech <harshanawana99x@gmail.com>
2023-11-22 23:53:38 +0530

    Edited some on app.js

--- app.js
+++ app.js
@@ -1,8 +1,65 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
 
-app.get('/', function (req, res) {
-  res.json({message:'Hello World'})
-})
+app.use(cors());
+app.use(bodyParser.json());
 
-app.listen(3000)
+// Sample data
+let Data = [
+  { id: 1, name: 'data 1' },
+  { id: 2, name: 'data 2' },
+];
+
+// Get all items
+app.get('/items', (req, res) => {
+  res.json(Data);
+});
+
+// Get an item by ID
+app.get('/items/:id', (req, res) => {
+  const itemId = parseInt(req.params.id);
+  const item = Data.find(item => item.id === itemId);
+
+  if (!item) {
+    return res.status(404).json({ error: 'Item not found' });
+  }
+
+  res.json(item);
+});
+
+// Create a new item
+app.post('/items', (req, res) => {
+  const newItem = req.body;
+  newItem.id = Data.length + 1;
+  Data.push(newItem);
+  res.status(201).json(newItem);
+});
+
+// Update an item by ID
+app.put('/items/:id', (req, res) => {
+  const itemId = parseInt(req.params.id);
+  const updatedItem = req.body;
+
+  const index = Data.findIndex(item => item.id === itemId);
+
+  if (index !== -1) {
+    Data[index] = { ...Data[index], ...updatedItem };
+    res.json(Data[index]);
+  } else {
+    res.status(404).json({ error: 'Item not found' });
+  }
+});
+
+// Delete an item by ID
+app.delete('/items/:id', (req, res) => {
+  const itemId = parseInt(req.params.id);
+  Data = Data.filter(item => item.id !== itemId);
+  res.json({ message: 'Item deleted successfully' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:3000`);
+});
